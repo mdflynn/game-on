1204b337ccdec73e519937b7c8297d482dcbecd0
Mike Flynn <mdflynn34@outlook.com>
2021-03-30 12:11:58 -0600

    add basic render

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const MainPage: React.FC = () => {
-    return (
-        <h1>Hello</h1>
-    )
-}
+  return <h1>Hello</h1>;
+};
 
 export default MainPage;
---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchBar: React.FC = () => {
+  return <h1>Search Bar</h1>;
+};
+
+export default SearchBar;
