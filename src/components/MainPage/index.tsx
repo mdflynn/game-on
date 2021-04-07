88bdaebdc06f6fda452bd551b9f2956d50cdfcea
Mike Flynn <mdflynn34@outlook.com>
2021-04-07 14:22:20 -0600

    render multiple placeholders

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
-import placeholder from "../../assets/got-placeholder.jpeg"
+import placeholder from "../../assets/got-placeholder.jpeg";
 
 const MainPage: React.FC = () => {
+
+  const cardInfo = [
+    { image: placeholder, title: "Lebron James", text: "Wanna be Goat" },
+    { image: placeholder, title: "James Harden", text: "Not a Goat" },
+    { image: placeholder, title: "Nikola Jokic", text: "GOAT" },
+    { image: placeholder, title: "Aaron Gordon", text: "2nd GOAT" },
+  ];
+
+  const renderCard = (
+    card: { image: string; title: string; text: string },
+    index: number
+  ) => {
+    return (
+      <Card style={{ width: "18rem" }} key={index}>
+        <Card.Img variant="top" src={card.image} />
+        <Card.Body>
+          <Card.Title>{card.title}</Card.Title>
+          <Card.Text>
+            {card.text}
+          </Card.Text>
+          <Button variant="primary">Game Details</Button>
+        </Card.Body>
+      </Card>
+    );
+  };
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={placeholder} />
-      <Card.Body>
-        <Card.Title>Ghost of Tsushima</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Game Details</Button>
-      </Card.Body>
-    </Card>
-  );
+    <div>{cardInfo.map(renderCard)}</div>
+  )
 };
 
 export default MainPage;
