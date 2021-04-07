ce444e6962abf92856bf6f5486edc93a33273164
Mike Flynn <mdflynn34@outlook.com>
2021-04-07 15:33:31 -0600

    update api call

--- src/api.tsx
+++ src/api.tsx
@@ -1,6 +1,7 @@
 export const getTwentyRandomGames = () => {
-    fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_KEY}&page=12`)
-    .then(response => response.json())
-}
+  return fetch(
+    `https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_API_KEY}&page=12`
+  ).then((response) => response.json());
+};
 
 // randomize page number
