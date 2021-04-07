93b38b43cf8b451b4885e41e7461c10ae58e9661
Mike Flynn <mdflynn34@outlook.com>
2021-04-07 14:07:27 -0600

    Add card placeholder

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import placeholder from "../../assets/got-placeholder.jpeg"
 
 const MainPage: React.FC = () => {
-  return <h1>Hello</h1>;
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={placeholder} />
+      <Card.Body>
+        <Card.Title>Ghost of Tsushima</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Game Details</Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default MainPage;
