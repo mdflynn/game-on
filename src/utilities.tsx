a423e03399c1386ddc5c0c85c750df1c62d01f17
Mike Flynn <mdflynn34@outlook.com>
2021-04-09 18:24:01 -0600

    import interface

--- src/utilities.tsx
+++ src/utilities.tsx
@@ -1,16 +1,4 @@
-type CleanedFetchData = {
-  id: number;
-  slug: string;
-  background_image: string;
-  esrb_rating: { id: number; name: string };
-  genres: Array<{ id: number; name: string; slug: string }>;
-  name: string;
-  metacritic: number;
-  parent_platforms: Array<{ id: number; name: string }>;
-  released: string;
-  short_screenshots: Array<{ id: number; image: string }>;
-  tags: Array<{ id: number; name: string; slug: string; language: string }>;
-};
+import { CleanedFetchData } from "./interfaces";
 
 export const cleanAllGamesFetchData = (data: Array<CleanedFetchData>) => {
   return data.map((game) => {
