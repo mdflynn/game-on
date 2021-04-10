afa30672fedd4d7b05f8ce9e9acf579c2a16141a
Mike Flynn <mdflynn34@outlook.com>
2021-04-09 18:25:22 -0600

    lint

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -2,38 +2,31 @@ import React, { useEffect, useState } from "react";
 import "./MainPage.scss";
 import { Button, Card } from "react-bootstrap";
 import { getTwentyRandomGames } from "../../api";
-import {cleanAllGamesFetchData} from "../../utilities";
-
+import { cleanAllGamesFetchData } from "../../utilities";
 import { CleanedFetchData } from "../../interfaces";
 
 const MainPage: React.FC = () => {
-  const [games, setGames] = useState<Array<CleanedFetchData>>([])
+  const [games, setGames] = useState<Array<CleanedFetchData>>([]);
 
-useEffect(() => {
-  getTwentyRandomGames()
-    .then(data => {
+  useEffect(() => {
+    getTwentyRandomGames().then((data) => {
       const cleanedData = cleanAllGamesFetchData(data.results);
-      setGames(cleanedData)
-    })
-}, [])
+      setGames(cleanedData);
+    });
+  }, []);
 
-  const renderCard = (
-    card: CleanedFetchData,
-    index: number
-  ) => {
+  const renderCard = (card: CleanedFetchData, index: number) => {
     return (
       <div className="row">
         <div className="col-sm-6">
-        <Card style={{ width: "18rem" }} key={index}>
-        <Card.Img variant="top" src={card.background_image} />
-        <Card.Body>
-          <Card.Title>{card.name}</Card.Title>
-          <Card.Text>
-            Metacritic: {card.metacritic}
-          </Card.Text>
-          <Button variant="primary">Game Details</Button>
-        </Card.Body>
-      </Card>
+          <Card style={{ width: "18rem" }} key={index}>
+            <Card.Img variant="top" src={card.background_image} />
+            <Card.Body>
+              <Card.Title>{card.name}</Card.Title>
+              <Card.Text>Metacritic: {card.metacritic}</Card.Text>
+              <Button variant="primary">Game Details</Button>
+            </Card.Body>
+          </Card>
         </div>
       </div>
     );
@@ -44,7 +37,7 @@ useEffect(() => {
       {games.length && console.log(games)}
       {games.map(renderCard)}
     </div>
-  )
+  );
 };
 
 export default MainPage;
